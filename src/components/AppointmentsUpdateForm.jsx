fe0e63da1e0261fdb4202e7ff0f7a8f25a202675
johannpereze <sebas.jsp@gmail.com>
2021-12-20 21:07:18 -0500

    Creo la función recursiva que se detiene cada que hay un error y luego continúa

--- src/components/AppointmentsUpdateForm.jsx
+++ src/components/AppointmentsUpdateForm.jsx
@@ -68,34 +68,63 @@ export default function AppointmentsUpdateForm() {
         redirect: 'follow'
     };
 
-    const updateAppointment = async (url, appointmentID, requestOptions, retryLimit = 10, retryCount = 0) => {
-        setTimeout(() => {
-            return fetch(`${url}${appointmentID}`, requestOptions).then((res) => {
-                console.log(res.status);
-                if (res.status === 429 && retryCount < retryLimit) {
-                    console.log("There was an error processing your fetch request. We are trying again.");
-                    setTimeout(() => {
-                        return updateAppointment(url, appointmentID, requestOptions, retryLimit, retryCount + 1);
-                    }, 22000);
-                } else {
-                    res.json().then((resp)=>console.log(resp));
-                }
-            });
-        }, 1500);
+    const updateAppointment = async (url, appointments, requestOptions, retryLimit = 50, retryCount = 0, index = 0) => {
+        if (index === appointments.length) { return }
+        else {
+            return fetch(`${url}${appointments[index]}`, requestOptions)
+                .then(async(res) => {
+                    const data = await res.json()
+                    console.log(data)
+                    if (data.data) {
+                        console.log('Se hizo el fetch correctamente');
+                        setTimeout(() => {
+                            return updateAppointment(url, appointments, requestOptions, retryLimit, retryCount, index)
+                        }, 200);
+                        index++
+                    } else {
+                        console.log('Se ejecuta el catch');
+                        console.log("data.error.code sería: ",data.error.code);
+                        console.log("retryCount", retryCount);
+                        console.log("retryLimit", retryLimit);
+                        if (data.error.code === 429 && retryCount < retryLimit) {
+                            console.log("Error 429, reintentando en 22 segundos");
+                            setTimeout(() => {
+                                return updateAppointment(url, appointmentsIDs, requestOptions, retryLimit, retryCount + 1, index);
+                            }, 22000);
+                        } else {
+                            console.log("Si veo esto es porque no lo estoy agarrando cuando es 429 ", res);
+                        }
+                    }
+
+
+                })
+
+        }
     }
+
+
+
 
 
 
 
     const handleAdd = () => {
         console.log(appointmentsIDs);
-        appointmentsIDs.map((appointment) => {
-            return updateAppointment(url, appointment, requestOptions, 10).then(function (json) {
-                raw = json;
-            }).catch(function (err) {
-                console.log(`There was a problem with the fetch operation: ${err.message}`);
-            });
-        })
+
+        updateAppointment(url, appointmentsIDs, requestOptions, 10)
+
+        // appointmentsIDs.map(async (appointment) => {
+        //     return setTimeout(() => {
+        //         updateAppointment(url, appointment, requestOptions, 10).then(function (json) {
+        //             raw = json;
+        //         }).catch(function (err) {
+        //             console.log(`There was a problem with the fetch operation: ${err.message}`);
+        //         });
+        //     }, 1500)
+        // });
+
+        //Hacer una función que llame a la siguiente en el then
+
 
     }
 
